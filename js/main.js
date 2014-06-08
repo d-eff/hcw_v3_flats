ccae54a6cb110e65aed1728016df9e07e2ef0bb9
David F <dfernandez06@gmail.com>
2014-06-08 18:12:48 -0400

    sorta working nav

--- js/main.js
+++ js/main.js
@@ -10,4 +10,44 @@
     toggleState('.page--main-nav ul', open, closed);
   })
 
+
+  
+  //onscroll
+  //
+  //if page height <= header bottom
+  //  header state = normal
+  //else
+  //  header state = fixed, translateY(-height)
+  //  check scroll dir -- window.pageYOffset > stored height, 
+  //    state=down
+  //  else
+  //    state=up, show this bitch
+  //
+  //pageheight = window.pageYOffset
+
+  var currentHeight = window.pageYOffset,
+      header = document.querySelector('.page--site-header'),
+      headerBottom = header.offsetTop + header.offsetHeight,
+      nav = document.querySelector('.page--main-nav');
+
+  window.addEventListener('scroll', function(){
+    console.log(currentHeight + " " + headerBottom);
+    var currState = nav.getAttribute('data-state');
+    if(currentHeight <= headerBottom){
+      if(currState !== 'normal'){
+        nav.setAttribute('data-state', 'normal');
+      }
+    } else {
+      if(currState !== 'fixed'){
+        nav.setAttribute('data-state', 'fixed');
+      }
+      if(window.pageYOffset < currentHeight){
+        nav.style.webkitTransform = 'translateY(0px)';
+      } else {
+        nav.style.webkitTransform = 'translateY(-'+nav.offsetHeight+'px)';
+      }
+    }
+    currentHeight = window.pageYOffset;
+  });
+
 })()
